1d01d502c7f88b65e17877f28acb2f098d3f62fb
Marwa_ke <kchaou.marwa94@gmail.com>
2019-01-24 12:55:05 +0100

    Add files via upload

---
+++ Newton_min_f0.m
@@ -0,0 +1,29 @@
+function [x_all,x,iter,cost_function]= Newton_min_f0(f0,grad_f0,H,x_init,x0,alpha,epsilon)
+% x_init : get the initialization value of x
+% x : is the value that minimizes f0
+% diff : Boolean variable that indicates if the stop criterion is achived
+% or not
+% iter : number of iteration needed for convergence
+% x_all : a Matrix of number of iterations by 2 where we save all the
+% x values  computed during the while loop
+% cost_function : an error function that controls the convergence of
+% gradient descent
+x=x_init;
+diff = true;
+iter = 0;
+x_all=[];
+cost_function = [];
+while diff
+    iter = iter+1;
+    f0_iter = double(f0(x(1),x(2)));
+    hess_f0= double(H(x(1),x(2)));
+    grad_f0_iter= double(grad_f0(x(1),x(2)));
+    d_iter = - inv(hess_f0)*grad_f0_iter;
+    x=x+d_iter;
+    x_all=[x_all;x'];
+    f0_iter_1 = double(f0(x(1),x(2)));
+    cost_function(iter) = norm(f0_iter - f0_iter_1);
+    diff = norm(f0_iter - f0_iter_1)>epsilon;
+end
+    
+end
---
+++ min_f0.m
@@ -0,0 +1,29 @@
+function [x_all,x,iter,cost_function]=min_f0(f0,grad_f0,x_init,x0,alpha,epsilon)
+% x_init : get the initialization value of x
+% x : is the value that minimizes f0
+% diff : Boolean variable that indicates if the stop criterion is achived
+% or not
+% iter : number of iteration needed for convergence
+% x_all : a Matrix of number of iterations by 2 where we save all the
+% x values  computed during the while loop
+% cost_function : an error function that controls the convergence of
+% gradient descent
+x=x_init; 
+diff = true; 
+iter = 0;
+x_all = [];
+cost_function = [];
+while diff
+    iter = iter+1;
+    f0_k_1= double(f0(x(1),x(2)));
+    grad_f0_val= double(grad_f0(x(1),x(2)));
+    x1 = x(1) - alpha*grad_f0_val(1);
+    x2 = x(2) - alpha*grad_f0_val(2);
+    x=[x1;x2];
+    x_all = [x_all;[x1 x2]];
+    f0_k = double(f0(x(1),x(2)));
+    cost_function(iter) = norm(f0_k - f0_k_1);
+    diff = norm(f0_k - f0_k_1)>epsilon;
+    end
+    
+end
